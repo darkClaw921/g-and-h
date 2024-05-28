53f8c202a72d7a597d0c6518c7b174a3f4ffa9e5
Igor Gerasimov <gerasimov.igor.98@icloud.com>
2024-05-28 14:58:08 +0300

    добавлено дублирование лида

--- redirect/workFlask_redirect.py
+++ redirect/workFlask_redirect.py
@@ -18,14 +18,21 @@ bitK = Bitrix(webhookKorob)
 def find_deal_k(dealID:str):
     deal = bitK.call('crm.deal.get', items={'id': dealID})
     return deal
+def find_lead_k(dealID:str):
+    deal = bitK.call('crm.lead.get', items={'id': dealID})
+    return deal
 
 def get_product_rows(dealID:str):
     products=bitK.call('crm.deal.productrows.get', items={'id': int(dealID)}, raw=True)['result']
     return products
+
+def get_product_rows_lead(leadID:str):
+    products=bitK.call('crm.lead.productrows.get', items={'id': int(leadID)}, raw=True)['result']
+    return products
   
 @api.route('/deal')
 @api.doc(description='Дублирет сделку на облако') 
-class Deal_redirect(Resource):
+class Lead_redirect(Resource):
     def get(self,):
         """План продаж"""
 
@@ -67,10 +74,51 @@ class Deal_redirect(Resource):
         dealID=bit.call('crm.deal.add', items=fields)
         print(dealID)
 
+        return 'OK'
+@api.route('/lead')
+@api.doc(description='Дублирет лид на облако') 
+class Deal_redirect(Resource):
+    def get(self,):
+        """План продаж"""
+
+        data = request.get_json() 
+        pprint(data)
+
+        return 'OK'
+    def post(self):
+        """сделка"""
 
 
+        pprint(request.__dict__)
+        data = request.get_json() 
+        pprint(data)
+        leadID=data['a'][1].split('=')[1]
+        print(f"{leadID=}")
+        dealK=find_lead_k(leadID)
+        pprint(dealK)
+        
+        
+        product=get_product_rows_lead(leadID)
+        pprint(product)
+        prod=''
+        for i in product:
+            prod+=i['PRODUCT_NAME']+'\n'
+        print(prod)
 
- 
+        fields={
+            # 'UF_CRM_1634020730':prod,
+            'TITLE':dealK['TITLE'],
+            'COMMENTS':prod,
+            'ASSIGNED_BY_ID':88,
+            'OPPORTUNITY':dealK['OPPORTUNITY'],
+            # 'CATEGORY_ID':0,
+
+
+        }
+        pprint(fields)
+        leadID=bit.call('crm.lead.add', items=fields)
+        print(leadID)
+
         return 'OK'
 
 if __name__ == '__main__':
